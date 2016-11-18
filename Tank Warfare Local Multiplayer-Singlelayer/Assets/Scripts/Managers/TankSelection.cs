fb2bede41145f40a8b40db061cb4b58dff1f16db
MrGamerStudios1 <brentedington@hotmail.com>
2016-11-17 21:06:07 -0500

    Added tank selection menu, different tanks

---
+++ Tank Warfare Local Multiplayer-Singlelayer/Assets/Scripts/Managers/TankSelection.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TankSelection : MonoBehaviour {
+
+    public Button HealthTButton;
+    public Button DamageTButton;
+    public GameObject HealthTankObj;
+    public GameObject DamageTankObj;
+    public GameManager Manager; //Game Manager
+    public GameObject m_TankHealth;
+    public GameObject m_TankDamage;
+
+    void Awake () {
+        Manager = GameObject.Find ("GameManager").GetComponent <GameManager>(); 
+
+    }
+
+
+	// Use this for initialization
+	void Start () {
+
+  
+        	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void extraHealthSelected()
+    {
+
+    }
+
+    public void extradamangeSelected()
+    {
+
+    }
+
+}
+    
+
